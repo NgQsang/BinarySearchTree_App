c6c7d2a3ca34035e98b265f71ccefbf69f300c5d
Nguyen Quang Sang <ITDSIU21113@student.hcmiu.edu.vn>
2024-06-21 10:13:19 +0700

    add Node.java file

---
+++ src/bst/Node.java
@@ -0,0 +1,13 @@
+// Node.java
+package bst;
+
+public class Node {
+    int data;
+    Node left, right;
+
+    public Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}

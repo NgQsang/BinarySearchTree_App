d1e90b2754c3073fd409c9d8d068bb5df764d521
Nguyen Quang Sang <ITDSIU21113@student.hcmiu.edu.vn>
2024-06-21 12:31:59 +0700

    new modify for Node.java

--- src/bst/Node.java
+++ src/bst/Node.java
@@ -1,13 +1,37 @@
-// Node.java
 package bst;
 
 public class Node {
-    int data;
-    Node left, right;
+    private int data;
+    private Node left;
+    private Node right;
 
     public Node(int data) {
         this.data = data;
-        left = null;
-        right = null;
+        this.left = null;
+        this.right = null;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public Node getLeft() {
+        return left;
+    }
+
+    public void setLeft(Node left) {
+        this.left = left;
+    }
+
+    public Node getRight() {
+        return right;
+    }
+
+    public void setRight(Node right) {
+        this.right = right;
     }
 }
